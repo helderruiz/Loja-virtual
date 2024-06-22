07405fa0c160d39af32e073f379459c15c60e610
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2024-06-22 11:53:35 -0300

    Concluido parte do html e css
    começando a criar os card dinamicamente com js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("javascript OK");
+// FAZENDO REQUISIÇÃO NO ARQUIVO JSON DOS TENIS
+const getProducts = async () => {
+  const response = await fetch("js/products.json");
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
