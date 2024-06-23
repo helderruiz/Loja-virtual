29b4545aaf4a740fe8c3b1e6e3103bcdcb3f281a
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2024-06-23 11:58:15 -0300

    Concluído a parte de crição de cards dinamicamente com js
    Começando a responsividade

--- js/script.js
+++ js/script.js
@@ -2,7 +2,33 @@
 const getProducts = async () => {
   const response = await fetch("js/products.json");
   const data = await response.json();
-  console.log(data);
+  //   console.log(data);
   return data;
 };
 
+const generateCard = async () => {
+  const products = await getProducts();
+
+  products.map((product) => {
+    // console.log(product)
+    let card = document.createElement("div");
+    card.classList.add("card__produtos");
+    card.innerHTML = `
+    <figure>
+        <img
+        src="images/${product.image}"
+        alt="${product.product_name}"/>
+    </figure>
+    <div class="card__produtos_detalhes">
+        <h4>${product.product_name}</h4>
+        <h5>${product.product_model}</h5>
+    </div>
+    <h6>R$ ${product.price}</h6>
+    `;
+
+    const listaProdutos = document.querySelector(".lista_produtos");
+    listaProdutos.appendChild(card);
+  });
+};
+
+generateCard();
